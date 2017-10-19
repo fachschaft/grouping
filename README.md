6a6481b76a0b4629ec8aec3aa62488c81cce6ffd
David-Elias Kuenstle <kuenstld@cs.uni-freiburg.de>
2017-10-19 16:43:37 +0200

    Initial commit with working version

---
+++ README.md
@@ -0,0 +1,53 @@
+# Grouping
+
+## What to do with this
+
+As a student association we organize several games for freshmen.
+The students have to be grouped for each of the games, where the number of groups can differ.
+Usually some students are late or games are added. Then the existing grouping shouldn't change.
+This is all we want and can do with this script.
+
+## Preparation
+
+You need *python 3* (we used version *3.6.2*) to run the script.
+All further requirements can be installed with *pip 3* via `pip3 install -r requirements.txt --user`.
+
+Check whether installation was successful via `python3 grouping.py --help`,
+which should print all available options of the script.
+
+## Examples
+
+1. randomly assign some students to two groups of a game.
+```
+$ python3 grouping.py --group-count 2 --player-name Tick Trick Track Donald Gustav
+```
+2. randomly assign a lot of students from a list to the groups of three games.
+```
+$ cat example/students.txt | python3 grouping.py --group-count 6 3 4 --player-name -
+```
+
+3. save the list as a pretty html file with custom group names and add some empty rows for latecomers.
+```
+$ cat example/students.txt |\
+  python3 grouping.py --group-count 6 3 4 --group-name A B C D E F --player-name - -n 50 --to html -o groups.html
+```
+
+4. only print the very first student's groups via custom template. 
+   Try some random sorting starts until he is two times in group 1 and then save the whole list.
+```
+$ echo 'First student's groups: {{#player.0.group}} {{name}} {{/player.0.group}}' > templates/first.plain 
+$ python3 grouping.py --group-count 2 3 --player-name Tick Trick Track Donald Gustav --template first --seed 40
+First students groups:  Group 2  Group 2 
+$ python3 grouping.py --group-count 2 3 --player-name Tick Trick Track Donald Gustav --template first --seed 41
+First students groups:  Group 2  Group 3 
+$ python3 grouping.py --group-count 2 3 --player-name Tick Trick Track Donald Gustav --template first --seed 42
+First students groups:  Group 1  Group 1 
+$ python3 grouping.py --group-count 2 3 --player-name Tick Trick Track Donald Gustav --template default --seed 42 -o groups.txt
+```
+
+## License
+
+This tool is developed by [David-Elias Künstle](https://github.com/dekuenstle) for the [student association technical faculty (Uni Freiburg)](https://fachschaft.tf) and published under the [EUPL 1.2](https://joinup.ec.europa.eu/page/eupl-text-11-12).
+Therefore you are free to use, modify and copy this work. 
+However copies and modifications have to be licensed under a compatible license and contain source code.
+
---
+++ grouping.py
@@ -0,0 +1,132 @@
+#! /usr/bin/env python3
+
+import os
+import sys
+import random
+import argparse
+import itertools
+
+from pymustache import mustache
+
+
+def random_group_index_gen(group_count):
+    while True:
+        random_group_indices = random.sample(range(group_count), group_count)
+        for ind in random_group_indices:
+            yield ind
+
+def random_group_indices(group_count, num, seed):
+    random.seed(seed)
+    return tuple(itertools.islice(random_group_index_gen(group_count), num))
+
+
+def player_groups(player_names, group_counts, group_names, seed):
+    n_players = len(player_names)
+    games = [random_group_indices(count, n_players, seed+ind)
+             for ind, count in enumerate(group_counts)]
+    player_groups = [[group_names[groups[player_ind]]
+                      for groups in games]
+                     for player_ind in range(n_players)]
+    return list(zip(player_names, player_groups))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--group-count",
+                        nargs="+",
+                        type=int,
+                        help="number of players in group. pass multiple for multiple games.")
+    parser.add_argument("-p", "--player-name",
+                        nargs="+",
+                        help="name of players instead of read from stdin.")
+    parser.add_argument("-n", "--player-count",
+                        type=int,
+                        help="number of players even without player names.")
+    parser.add_argument("-l", "--group-name",
+                        nargs="+",
+                        help="name of groups.")
+    parser.add_argument("--seed",
+                        type=int,
+                        default=210591,
+                        help="seed for group shuffling.")
+    parser.add_argument("-t", "--to",
+                        default="plain",
+                        help="export format. uses template <template>.<format>.")
+    parser.add_argument("--template",
+                        default="default",
+                        help="template name. uses template <template>.<format>.")
+    parser.add_argument("--template-dir",
+                        help="directory of template files. defaults to 'templates' folder in project directory.")
+    parser.add_argument("-o", "--output",
+                        help="Write output to FILE instead of stdout.")
+    return parser.parse_args()
+
+
+def game_groups_from_players(players):
+    assert len(players) > 0, "requires at least one player"
+
+    games = [{} for i in players[0][1]]
+    for game_index, game_groups in enumerate(games):
+        for player_name, player_groups in players:
+            group_name = player_groups[game_index]
+            if group_name in game_groups.keys():
+                game_groups[group_name].append(player_name)
+            else:
+                game_groups[group_name] = [player_name]
+    return games
+
+
+def render(template_path, player_groups):
+    game_groups = game_groups_from_players(player_groups)
+    context = {'player': [{'name': pname,
+                           'group': [{'name': gname}
+                                     for gname in groups]}
+                          for pname, groups in player_groups],
+               'game': [{'group': [{'name': group_name,
+                                    'player': [{'name': player_name}
+                                               for player_name in players]}
+                                   for group_name, players in sorted(groups.items())]}
+                       for groups in game_groups]}
+
+    mustache.filters['len'] = lambda list_in: len(list_in)
+    mustache.filters['addone'] = lambda x: x + 1
+    with open(template_path, 'r') as f:
+        template = f.read()
+        out = mustache.render(template, context)
+    return out
+
+
+def main():
+    args = parse_args()
+
+    player_names = args.player_name
+    player_count = args.player_count
+    if player_names is None:
+        player_names = []
+    elif len(player_names) == 1 and '-' in player_names :
+        player_names = [line.rstrip() for line in sys.stdin]
+    if player_count is not None:
+        player_names += ['' for i in range(len(player_names), max(len(player_names), player_count))]
+
+    group_counts = args.group_count
+    group_names = args.group_name
+    if group_names is None:
+        group_names = []
+    group_names += ['Group {}'.format(i+1) for i in range(len(group_names), max(group_counts))]
+
+    template_dir = args.template_dir
+    if template_dir is None:
+        template_dir = os.path.join(os.path.dirname(__file__),'templates')
+    template_path = os.path.join(template_dir, '{}.{}'.format(args.template, args.to))
+
+    player_group_list = player_groups(player_names, group_counts, group_names, args.seed)
+    out = render(template_path, player_group_list)
+    if args.output is None:
+       print(out)
+    else:
+        with open(args.output, 'w') as f:
+            f.write(out)
+
+
+if __name__ == '__main__':
+    main()
